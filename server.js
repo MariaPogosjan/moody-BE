382b4df36e20aa10794598ddf2013dec114ecd93
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-06-02 15:03:54 +0200

    Created user and login

--- server.js
+++ server.js
@@ -1,25 +1,133 @@
+/* eslint-disable no-useless-escape */
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import listEndpoints from 'express-list-endpoints'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt'
+import dotenv from 'dotenv'
+import cloudinaryFramework from 'cloudinary'
+import multer from 'multer'
+import cloudinaryStorage from 'multer-storage-cloudinary'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
+dotenv.config()
+const cloudinary = cloudinaryFramework.v2;
+cloudinary.config({
+  cloud_name: 'doxvkrxqc',
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+const storage = cloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'avatars',
+    allowedFormats: ['jpg', 'png'],
+    transformation: [{ width: 500, height: 500, crop: 'limit' }]
+  }
+})
+const parser = multer({ storage })
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/moody"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 12,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    validate: {
+      validator: (value) => {
+        return /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(value)
+      },
+      message: 'Please enter a valid email address'
+    }
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send(listEndpoints(app))
+})
+
+app.post('/users', async (req, res) => {
+  const { username, email, password } = req.body
+  try {
+    const salt = bcrypt.genSaltSync()
+    const newUser = await new User({
+      username,
+      email,
+      password: bcrypt.hashSync(password, salt)
+    }).save()
+    res.json({
+      success: true,
+      // eslint-disable-next-line no-underscore-dangle
+      userId: newUser._id,
+      username: newUser.username,
+      accessToken: newUser.accessToken
+    })
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: 'Could not create user',
+      error
+    })
+  }
+})
+
+app.post('/sessions', async (req, res) => {
+  const { emailOrUsername, password } = req.body
+  try {
+    const user = await User.findOne({
+      $or: [
+        { email: emailOrUsername },
+        { username: emailOrUsername }
+      ]
+    })
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.json({
+        success: true,
+        // eslint-disable-next-line no-underscore-dangle
+        userId: user._id,
+        username: user.username,
+        accessToken: user.accessToken
+      })
+    } else {
+      res.status(404).json({ success: false, message: 'User not found' })
+    }
+  } catch (error) {
+    res.status(400).json({ success: false, message: 'Invalid request', error })
+  }
+})
+app.post('/avatars', parser.single('image'), async (req, res) => {
+  res.json({
+    imageUrl: req.file.path, imageId: req.file.filename
+  })
 })
 
 // Start the server
